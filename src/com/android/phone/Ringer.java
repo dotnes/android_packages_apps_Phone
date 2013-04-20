9f3af00489dc44f3d472f6758a880861c26d6e85
igio90 <giovanni.rocca.90@gmail.com>
2013-04-20 04:25:44 +0200

    Revert "Custom Vibrations (3/6)"
    This reverts commit 17dc94690a9669189b7763f04409ac7c7a39a4bc.

--- src/com/android/phone/Ringer.java
+++ src/com/android/phone/Ringer.java
@@ -22,7 +22,6 @@ import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
-import android.media.VibrationPattern;
 import android.net.Uri;
 import android.provider.Settings;
 import android.os.Handler;
@@ -38,8 +37,6 @@ import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
-
-import java.util.Calendar;
 
 import com.android.internal.telephony.Phone;
 /**
@@ -62,10 +59,8 @@ public class Ringer {
 
     // Uri for the ringtone.
     Uri mCustomRingtoneUri = Settings.System.DEFAULT_RINGTONE_URI;
-    Uri mCustomVibrationUri = Settings.System.DEFAULT_VIBRATION_URI;
 
     Ringtone mRingtone;
-    VibrationPattern mVibrationPattern;
     Vibrator mVibrator;
     AudioManager mAudioManager;
     IPowerManager mPowerManager;
@@ -174,10 +169,6 @@ public class Ringer {
             }
 
             if (shouldVibrate() && mVibratorThread == null) {
-                mVibrationPattern = new VibrationPattern(mCustomVibrationUri, mContext);
-                if (mVibrationPattern.getPattern() == null) {
-                    mVibrationPattern = VibrationPattern.getFallbackVibration(mContext);
-                }
                 mContinueVibrating = true;
                 mVibratorThread = new VibratorThread();
                 if (DBG) log("- starting vibrator...");
@@ -298,7 +289,6 @@ public class Ringer {
 
             if (mVibratorThread != null) {
                 if (DBG) log("- stopRing: cleaning up vibrator thread...");
-                mVibrationPattern.stop();
                 mContinueVibrating = false;
                 mVibratorThread = null;
             }
@@ -310,8 +300,8 @@ public class Ringer {
     private class VibratorThread extends Thread {
         public void run() {
             while (mContinueVibrating) {
-                mVibrationPattern.play();
-                SystemClock.sleep(mVibrationPattern.getLength() + PAUSE_LENGTH);
+                mVibrator.vibrate(VIBRATE_LENGTH);
+                SystemClock.sleep(VIBRATE_LENGTH + PAUSE_LENGTH);
             }
         }
     }
@@ -358,16 +348,6 @@ public class Ringer {
     void setCustomRingtoneUri (Uri uri) {
         if (uri != null) {
             mCustomRingtoneUri = uri;
-        }
-    }
-
-    /**
-     * Sets the vibration uri in preparation for vibrating.
-     * This uri is defaulted to the phone-wide default vibration.
-     */
-    void setCustomVibrationUri (Uri uri) {
-        if (uri != null) {
-            mCustomVibrationUri = uri;
         }
     }
 
@@ -444,26 +424,5 @@ public class Ringer {
 
     private static void log(String msg) {
         Log.d(LOG_TAG, msg);
-    }
-
-    private boolean inQuietHours() {
-        boolean quietHoursEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.QUIET_HOURS_ENABLED, 0) != 0;
-        int quietHoursStart = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.QUIET_HOURS_START, 0);
-        int quietHoursEnd = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.QUIET_HOURS_END, 0);
-        if (quietHoursEnabled && (quietHoursStart != quietHoursEnd)) {
-            // Get the date in "quiet hours" format.
-            Calendar calendar = Calendar.getInstance();
-            int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
-            if (quietHoursEnd < quietHoursStart) {
-                // Starts at night, ends in the morning.
-                return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
-            } else {
-                return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
-            }
-        }
-        return false;
     }
 }
